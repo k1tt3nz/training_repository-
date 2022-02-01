725b89fbe9a395f457dbcf7f279aba46e1ff725b
k1tt3nz <k1tt3nzpink@gmail.com>
2022-02-02 01:41:17 +0300

    add vector.h/.c

---
+++ libs/data_struct/dsu/vector.c
@@ -0,0 +1,5 @@
+//
+// Created by k1tt3nz on 02.02.2022.
+//
+
+#include "vector.h"
---
+++ libs/data_struct/dsu/vector.h
@@ -0,0 +1,8 @@
+//
+// Created by k1tt3nz on 02.02.2022.
+//
+
+#ifndef COURSE_VECTOR_H
+#define COURSE_VECTOR_H
+
+#endif //COURSE_VECTOR_H
