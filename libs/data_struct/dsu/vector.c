f63d1401b5ff635bfefe76960837acaa20462785
k1tt3nz <k1tt3nzpink@gmail.com>
2022-02-02 12:28:40 +0300

    add struct vector

--- libs/data_struct/dsu/vector.c
+++ libs/data_struct/dsu/vector.c
@@ -1,5 +1,1 @@
-//
-// Created by k1tt3nz on 02.02.2022.
-//
-
 #include "vector.h"
--- libs/data_struct/dsu/vector.h
+++ libs/data_struct/dsu/vector.h
@@ -1,8 +1,13 @@
-//
-// Created by k1tt3nz on 02.02.2022.
-//
-
 #ifndef COURSE_VECTOR_H
 #define COURSE_VECTOR_H
+
+#include <stdio.h>
+
+typedef struct vector{
+    int *data;          //Указатель на элементы вектора
+    size_t size;        //Размер вектора
+    size_t capacity;    //Вместимость вектора
+};
+
 
 #endif //COURSE_VECTOR_H
